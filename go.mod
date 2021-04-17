1634ef288cf770f9e442e9ed07c8ff8bf6618d32
xtaci <daniel820313@gmail.com>
2021-04-17 23:23:34 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/xtaci/safebox
+
+go 1.16
+
+require (
+	github.com/gdamore/tcell/v2 v2.2.0
+	github.com/rivo/tview v0.0.0-20210312174852-ae9464cc3598
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+// Demo code for the List primitive.
+package main
+
+import (
+	"github.com/rivo/tview"
+)
+
+func main() {
+	app := tview.NewApplication()
+
+	newPrimitive := func(text string) tview.Primitive {
+		return tview.NewTextView().
+			SetTextAlign(tview.AlignCenter).
+			SetText(text)
+	}
+	menu := tview.NewList().
+		AddItem("Create Main Key", "Create a main key to derive others", 'm', nil).
+		AddItem("Derive Key", "Derive keys from main key", 'd', nil).
+		AddItem("Quit", "Press to exit", 'q', func() {
+			app.Stop()
+		})
+
+	main := newPrimitive("Main content")
+	sideBar := newPrimitive("Side Bar")
+
+	grid := tview.NewGrid().
+		SetRows(3, 0, 3).
+		SetColumns(30, 0, 30).
+		SetBorders(true).
+		AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false).
+		AddItem(newPrimitive("Footer"), 2, 0, 1, 3, 0, 0, false)
+
+	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
+	grid.AddItem(menu, 0, 0, 0, 0, 0, 0, true).
+		AddItem(main, 1, 0, 1, 3, 0, 0, false).
+		AddItem(sideBar, 0, 0, 0, 0, 0, 0, false)
+
+	// Layout for screens wider than 100 cells.
+	grid.AddItem(menu, 1, 0, 1, 1, 0, 100, true).
+		AddItem(main, 1, 1, 1, 1, 0, 100, false).
+		AddItem(sideBar, 1, 2, 1, 1, 0, 100, false)
+
+	if err := app.SetRoot(grid, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+}
