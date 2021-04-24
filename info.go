49d0edd67dbed98e8fbfbb3af1fa4e94c2b733c8
xtaci <daniel820313@gmail.com>
2021-04-24 23:35:37 +0800

    optimize info window

--- info.go
+++ info.go
@@ -1,25 +1,34 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"strings"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/rivo/tview"
 )
 
 var infoString = `
-MASTER KEY CREATED:
+Master Key SHA256:
 [green]%v
 
-[-:-:-]
-NUM LABLED KEYS:
+[-:-:-]Master Keys Created At:
+[green]%v
+
+[-:-:-]Number of keys with label:
 [green]%v
 `
 
-var keyLoadedString = `
-Use ArrowKeys ← ↑ → ↓ To Select Keys
+var instructionsString = `
+Instructions
 
-Derived keys will be uncovered when selected.
+1) Use ArrowKeys ← ↑ → ↓ To Select Keys, masks on derived keys will be uncovered when selected.
+
+2) Press Enter on 'DERIVED KEY' column to export.
+
+3) Press Enter on 'NAME' column to set label.
 `
 
 func infoWindow() (content *tview.Flex) {
@@ -32,20 +41,24 @@ func infoWindow() (content *tview.Flex) {
 
 func refreshInfo() {
 	layoutInfo.Clear()
-	layoutInfo.AddItem(infoText(), 0, 8, false)
+	layoutInfo.AddItem(infoMasterKey(), 0, 1, false)
 
 	// scroll to end to align text to bottom
-	keyloadtv := keyLoadedText()
-	layoutInfo.AddItem(keyloadtv, 0, 2, false)
-	keyloadtv.ScrollToEnd()
+	operations := infoInstructions()
+	layoutInfo.AddItem(operations, 0, 1, false)
+	operations.ScrollToEnd()
 }
 
-func infoText() *tview.TextView {
+// master key info textview
+func infoMasterKey() *tview.TextView {
 	textview := tview.NewTextView()
 	textview.SetDynamicColors(true)
+	textview.SetWrap(true)
 
 	if masterKey != nil {
+		md := sha256.Sum256(masterKey.masterKey[:])
 		fmt.Fprintf(textview, infoString,
+			hexutil.Encode(md[:]),
 			time.Unix(masterKey.createdAt, 0).Local().Format(time.RFC822),
 			len(masterKey.labels),
 		)
@@ -53,12 +66,15 @@ func infoText() *tview.TextView {
 	return textview
 }
 
-func keyLoadedText() *tview.TextView {
+// instructions info textview
+func infoInstructions() *tview.TextView {
 	textview := tview.NewTextView()
 	textview.SetDynamicColors(true)
+	textview.SetWrap(true)
+	textview.SetWordWrap(true)
 	if masterKey != nil {
-		fmt.Fprint(textview, "\n\n\n\n\n\n\n\n\n")
-		fmt.Fprint(textview, keyLoadedString)
+		fmt.Fprint(textview, strings.Repeat("\n", 100))
+		fmt.Fprint(textview, instructionsString)
 	}
 	return textview
 }
