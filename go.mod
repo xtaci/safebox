ae0dd18e2d01ef90150c1429cd9b67562547cf7a
ace.xinxuwang@gmail.com <ace.xinxuwang@gmail.com>
2021-04-29 18:15:44 +0800

    cosmos

--- go.mod
+++ go.mod
@@ -3,13 +3,21 @@ module github.com/xtaci/safebox
 go 1.16
 
 require (
-	github.com/btcsuite/btcd v0.20.1-beta
-	github.com/btcsuite/btcutil v0.0.0-20190425235716-9e5f4b9a998d
+	github.com/btcsuite/btcd v0.21.0-beta
+	github.com/btcsuite/btcutil v1.0.2
+	github.com/cosmos/cosmos-sdk v0.42.4
+	github.com/cosmos/go-bip39 v1.0.0
 	github.com/ethereum/go-ethereum v1.10.2
 	github.com/gdamore/tcell/v2 v2.2.0
 	github.com/rivo/tview v0.0.0-20210312174852-ae9464cc3598
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	github.com/spf13/cobra v1.1.3 // indirect
 	github.com/stretchr/testify v1.7.0
 	github.com/templexxx/xorsimd v0.4.1
+	github.com/tendermint/tendermint v0.34.10 // indirect
 	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc
 )
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.33.2
+
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
---
+++ plugins/xprt/persistence.go
@@ -0,0 +1,77 @@
+package xprt
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/go-bip39"
+	"github.com/xtaci/safebox/qrcode"
+	"strings"
+)
+
+type PersistenceExporter struct{}
+
+func (exp *PersistenceExporter) Name() string {
+	return "Persistence"
+}
+
+func (exp *PersistenceExporter) KeySize() int {
+	return 32
+}
+
+func (exp *PersistenceExporter) Export(key []byte) ([]byte, error) {
+	if len(key) != 32 {
+		return nil, errors.New("invalid key length")
+	}
+
+	mnemonic, err := bip39.NewMnemonic(key)
+	if err != nil {
+		return nil, err
+	}
+
+	configuration := types.GetConfig()
+	configuration.SetBech32PrefixForAccount(Bech32MainPrefix, Bech32MainPrefix+types.PrefixPublic)
+	configuration.SetCoinType(Bip44CoinType)
+
+	kb := keyring.NewInMemory()
+	hdPath := hd.NewFundraiserParams(0, types.GetConfig().GetCoinType(), 0).String()
+	account, err := kb.NewAccount(string(key), mnemonic, "", hdPath, hd.Secp256k1)
+	if err != nil {
+		return nil, err
+	}
+
+	//Check Address
+	_, err = types.AccAddressFromBech32(account.GetAddress().String())
+	if err != nil {
+		return nil, err
+	}
+	mnemonicSlice := strings.Split(mnemonic, " ")
+	var out bytes.Buffer
+	fmt.Fprintf(&out,
+		`Persistence Account: %v
+HD derivation path: %v
+Public Key QR Code:
+%v
+Mnemonic: %v
+Mnemonic QR Code(part1):
+%v
+Mnemonic QR Code(part2):
+%v`,
+		account.GetAddress().String(),
+		hdPath,
+		qrcode.GenerateQRCode(account.GetPubKey().String()),
+		mnemonic,
+		qrcode.GenerateQRCode(strings.Join(mnemonicSlice[0:len(mnemonicSlice)/2], " ")),
+		qrcode.GenerateQRCode(strings.Join(mnemonicSlice[len(mnemonicSlice)/2:], " ")),
+	)
+
+	return out.Bytes(), nil
+}
+
+const (
+	Bech32MainPrefix = "persistence"
+	Bip44CoinType    = 750
+)
